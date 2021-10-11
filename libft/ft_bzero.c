a4cca9def0eb3c44a38bea41430aa1ae04af44ee
kmoroz <ksmorozova@hotmail.com>
2021-10-11 17:00:29 +0100

    initial

---
+++ libft/ft_bzero.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_bzero.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/10/29 13:33:37 by ksmorozo      #+#    #+#                 */
+/*   Updated: 2020/12/02 23:58:09 by anonymous     ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	count;
+	char	*str;
+
+	str = s;
+	count = 0;
+	while (count != n)
+	{
+		str[count] = '\0';
+		count++;
+	}
+}
