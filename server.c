28cd24a30a3a55919d06d5fb002aa63cb8e2b7ff
ksmoroz <ksmorozo@student.codam.nl>
2021-11-07 15:26:46 +0100

    make server usleep the same amount as client, replace sigaction with signal

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: ksmorozo <ksmorozo@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/20 12:15:03 by ksmorozo      #+#    #+#                 */
-/*   Updated: 2021/10/12 18:17:17 by ksmorozo      ########   odam.nl         */
+/*   Updated: 2021/11/07 14:59:12 by ksmorozo      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,12 +34,15 @@ void	display_binary_to_dec(int binary)
 	ft_putchar_fd(decimal, 1);
 }
 
-void	interpret(char c)
+void	interpret(int signal)
 {
 	static char	message[8];
 	static int	i;
 
-	message[i] = c;
+	if (signal == SIGUSR1)
+		message[i] = '0';
+	else
+		message[i] = '1';
 	i++;
 	if (i == 8)
 	{
@@ -49,34 +52,19 @@ void	interpret(char c)
 	}
 }
 
-void	handle_sigusr1(int i)
-{
-	if (i == SIGUSR1)
-		interpret('0');
-}
-
-void	handle_sigusr2(int i)
-{
-	if (i == SIGUSR2)
-		interpret('1');
-}
-
 int	main(void)
 {
 	int					pid;
-	struct sigaction	sa_1;
-	struct sigaction	sa_2;
 
 	pid = getpid();
 	if (pid < 0)
 		return (-1);
 	ft_putnbr_fd(pid, 0);
 	ft_putchar_fd('\n', 0);
-	sa_1.sa_handler = &handle_sigusr1;
-	sa_2.sa_handler = &handle_sigusr2;
+	signal(SIGUSR1, interpret);
+	signal(SIGUSR2, interpret);
 	while ("The prophecy is true")
 	{
-		sigaction(SIGUSR1, &sa_1, NULL);
-		sigaction(SIGUSR2, &sa_2, NULL);
+		usleep(300);
 	}
 }
